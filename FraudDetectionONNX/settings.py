0214ef82b8ffeb4f041ffce1d5ca955b11e00595
JeevanNaikOp <naikjeevan97@gmail.com>
2023-04-10 14:48:29 +0530

    debugging

--- FraudDetectionONNX/settings.py
+++ FraudDetectionONNX/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x0#c943fa5+5c-j!f5f-e3_fl#af3@+ne$!abb$-21z0#i+2i('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
