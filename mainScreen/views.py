c2f1a7a8c26063baae552ae61d220641ab4165f9
JeevanNaikOp <naikjeevan97@gmail.com>
2023-04-10 12:58:09 +0530

    added css code

--- mainScreen/views.py
+++ mainScreen/views.py
@@ -9,9 +9,6 @@ def index(request):
     form =ReviewFields()
     if request.method == 'POST':
         form = ReviewFields(request.POST)
-        if form.is_valid():
-            print('form is valid')
-            print("json",form.cleaned_data['jsonfield'])
     return render(request,'index.html',{'form':form})
 
 def result(request):
@@ -31,5 +28,9 @@ def result(request):
     print(data)
     res = requests.post('https://192.86.32.113:19443/zosConnect/services/ONNX?action=invoke', auth=auth, data=data, headers=header,verify=False)
     json_out = (json.loads(res.text))
-    json_out = json_out['FRAUDRES']['FRAUDRES']['XDATA2'][:2]
-    return render(request, 'result.html',{"res":json_out})
+    json_out = json_out['FRAUDRES']['FRAUDRES']['XDATA2'][0]
+
+    form =ReviewFields()
+    if request.method == 'POST':
+        form = ReviewFields(request.POST)
+    return render(request, 'result.html',{'form':form,"inp":data ,"res":json_out})
